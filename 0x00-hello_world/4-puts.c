69a548850c568c14d43bb2a9dcbe5a273e784dbf
hailu <hailugrit@gmail.com>
2022-07-03 06:39:52 -0700

    24 00

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 *main-entry point
 *return: always 0(success)
 */
-int main(void)
+int main(viid)
 {
-	puts("\"programming is like building amultilingual puzzle");
+	puts("\"programming is like building multilingual puzzle");
 	return(0)
-}
+} 
