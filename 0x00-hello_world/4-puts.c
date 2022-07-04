44179c12bf671ac4203e24d7f017333604cac0bc
hailu <hailugrit@gmail.com>
2022-07-04 05:51:40 -0700

    finally

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main -entry point
- *
- * return: always 0(success)
+ * main-eny point
+ * Return:always 0 (success)
  */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
+
 	return (0);
-}  
+}
