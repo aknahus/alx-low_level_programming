2ba12f1c87a3840b7b525c6bb79784f994c094ae
hailu <hailugrit@gmail.com>
2022-07-04 11:27:14 -0700

    trial

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main-eny point
- * Return:always 0 (success)
- */
+* main -entry point
+* Return: Always 0 (sucess)
+*/
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
